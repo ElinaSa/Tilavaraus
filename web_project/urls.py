0f569628ebfcfb6c295423dfb9c02bb81fd3e095
Elina Saranki <156064412+ElinaSa@users.noreply.github.com>
2025-05-22 15:11:48 +0300

    siivottu turhia rivejä pois

--- web_project/urls.py
+++ web_project/urls.py
@@ -16,7 +16,6 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
-#from tilavaraus import views as Tilavaraus
 from django.contrib.auth import views as auth_views
 
 urlpatterns = [
