949aceaec062b6e029b4127e71ef25eaf6437c82
Elina Saranki <156064412+ElinaSa@users.noreply.github.com>
2026-05-21 17:42:51 +0300

    Päivitetty requirement.txt ja siivottu settings.py:stä ylimääräisiä rivejä

--- web_project/settings.py
+++ web_project/settings.py
@@ -114,8 +114,7 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
+       
         'ENGINE': os.getenv('DB_ENGINE'),
         'NAME' : os.getenv('DB_NAME'),
         'USER' : os.getenv('DB_USER'),
