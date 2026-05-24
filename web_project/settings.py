e56950e6c988211b2c67105d2e4b5f77b1a241f8
Elina Saranki <156064412+ElinaSa@users.noreply.github.com>
2026-05-24 19:04:50 +0300

    siivousta

--- web_project/settings.py
+++ web_project/settings.py
@@ -124,18 +124,10 @@ WSGI_APPLICATION = 'web_project.wsgi.application'
 #     'default':dj_database_url.config(default=os.getenv('DATABASE_URL')),
 # }
 DATABASES = {
-    'default': 
-
-        dj_database_url.config(default=os.getenv('DATABASE_URL')),
-        # 'ENGINE': os.getenv('DB_ENGINE'),
-        # 'NAME' : os.getenv('DB_NAME'),
-        # 'USER' : os.getenv('DB_USER'),
-        # 'PASSWORD' : os.getenv('DB_PASSWORD'),
-        # 'HOST' : os.getenv('DB_HOST'),
-        # 'PORT' : os.getenv('DB_PORT'), 
-    
+    'default':dj_database_url.config(default=os.getenv('DATABASE_URL')),
 }
-print(os.getenv('DB_ENGINE'))
+print(os.getenv('DATABASE_URL'))
+#print(os.getenv('DB_ENGINE'))
 
 # Password validation
 # https://docs.djangoproject.com/en/5.2/ref/settings/#auth-password-validators
