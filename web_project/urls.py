1a27ea5556dac69f778e5eb2f2cfdbd9e828f6e8
Elina Saranki <156064412+ElinaSa@users.noreply.github.com>
2026-05-25 11:26:33 +0300

    seuraava kokeilu

--- web_project/urls.py
+++ web_project/urls.py
@@ -41,9 +41,9 @@ schema_view = get_schema_view(
 # TODO: tarkista kommentiksi muutetut auth.viewsit ja viewsit kumpaan urlspatternsiin kuuluu!
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('', include('tilavaraus.urls')), 
+    #path('', include('tilavaraus.urls')), 
     path('login/', auth_views.LoginView.as_view(template_name='tilavaraus/login.html'), name='login'),
-    #path('api/', include('tilavaraus.urls')),
+    path('api/', include('tilavaraus.urls')),
     path('swagger/', schema_view.with_ui('swagger', cache_timeout=0), name='schema-swagger-ui'),
     path('redoc/', schema_view.with_ui('redoc', cache_timeout=0), name='schema-redoc'), 
     
